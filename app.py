0974484bc5956cfc30a96bfb041f8cedf642d3d9
Parwez Alam <parwezalam9582@gmail.com>
2026-06-08 13:08:22 +0530

    Fix greeting message in hello route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
 
 @app.route('/how-are-you')
 def hello():
-    return 'I am good, how about you? Check'
+    return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
